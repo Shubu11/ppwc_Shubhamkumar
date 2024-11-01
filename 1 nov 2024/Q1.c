b63cb2e78fc549911b4c4bf04bdd5c538428b2b7
Shubham Kumar <133413039+Shubu11@users.noreply.github.com>
2024-11-01 23:41:25 +0530

    Add files via upload

---
+++ 1 nov 2024/Q1.c
@@ -0,0 +1,21 @@
+//Q1. Find the length of the string without using the strlen function??
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+#define NILL ('\0')
+int stringLen(char *str){
+    int len=0;
+    while(str[len]!=NILL){
+        len++;
+    }
+    return len;
+}
+int main(){
+    char s[MAX];
+    printf("Enter a String: ");
+    scanf("%[^\n]s",s);
+    int len=stringLen(s);
+    printf("The length of the String is: %d",len);
+    return 0;
+}
